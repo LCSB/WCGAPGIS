6ac1e7db8f3d227379869e26154ac9649eb146cf
yixiaoyi <yi_xiaoy@163.com>
2017-12-22 18:27:44 +0800

    基本完成（照片除外）

--- server/controllers/hello.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = async (ctx, next)=> {
-  ctx.state.data = "Hello World !"
-}
--- server/controllers/templet.js
+++ server/controllers/templet.js
@@ -1,6 +1,5 @@
 const { mysql } = require('../qcloud')
 
-
 //查询所有模板列表
 async function get(ctx, next) {
   var rs = await mysql("templet")
@@ -8,37 +7,67 @@ async function get(ctx, next) {
   for(var item of rs) {
     tempId = item.id
     var columns = await mysql("templet_column").where({ "temp_id": tempId })
+
     templetList.push({
       "temp_id":item.id,
       "temp_name":item.temp_name,
       "columns": columns
     })
-
   }
 
   ctx.body = templetList
 }
 
-
-//插入新模板消息
-async function post(ctx, next) {
-  var name = ctx.body.name
-  var columns = ctx.body.columns
-  var templet = {
-    temp_name:name
-  }
-  var rs = await mysql("templet").returning("id").insert(templet)
-  
-  if (rs > 0) {
-    ctx.body = 'success'
+//增加模板
+async function post(ctx,next) {
+  var params = ctx.request.body
+  var temp_name = params.temp_name
+  var columns = params.columns
+  var res = await mysql("templet").where({ "temp_name": temp_name}).first()
+  if (res != null) {
+     ctx.body = "模板已经存在！"
   } else {
-    ctx.body = 'error'
+    var templet = {
+      temp_name: temp_name
+    }
+    var id = await mysql("templet").returning("id").insert(templet)
+    if (id > 0) {
+      var fixedId = await mysql("fixed_attribute").returning("id").insert({ temp_id: id })
+      if (fixedId > 0) {
+        var column_list = []
+        for (var column of columns) {
+          column_list.push({
+            temp_id: id,
+            column_comment: column.column_comment,
+            column_datatype: column.column_datatype
+          })
+        }
+
+        //动态字段
+        var rs = await mysql("templet_column").returning(['id', 'temp_id', 'column_comment', 'column_datatype']).insert(column_list)
+        if (rs > 0) {
+          var columns = await mysql("templet_column").where({ "temp_id": id })
+          var templetList = []
+          templetList.push({
+            "temp_id": id,
+            "temp_name": temp_name,
+            "columns": columns
+          })
+          ctx.body = templetList
+        } else {
+          ctx.body = "error"
+        }
+
+      }
+
+    }
   }
   
 }
 
 
 module.exports = {
-  get,
-  post
+  post,
+  get
+  
 }
